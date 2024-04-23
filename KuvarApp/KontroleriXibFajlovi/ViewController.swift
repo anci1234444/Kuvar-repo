e53545ca9077370542d940d0fad810a47955c25c
Ana Asceric <anaasceric@Anas-Mac-mini.local>
2024-04-23 12:19:00 +0200

    Setup project

---
+++ KuvarApp/KontroleriXibFajlovi/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  KuvarApp
+//
+//  Created by Ana Asceric on 22.4.24..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var testLabel: UILabel!
+    
+    var coordinator: MainCoordinator?
+    
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
